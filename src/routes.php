f76c3b4f10b78844bff438e1c86d6d4eb9c2d796
sohelrana820 <me.sohelrana@gmail.com>
2016-10-24 17:30:19 +0600

    Application routes has been configured

--- src/routes.php
+++ src/routes.php
@@ -1,8 +1,7 @@
 <?php
 // Routes
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
 
-$app->get('/[{name}]', function ($request, $response, $args) {
-    $this->flash->addMessage('success', 'Slim Skeleton ready to Go!');
-    $msg = $this->flash->getMessages();
-    return $this->view->render($response, 'index.twig', ['message' => $msg]);
-});
+require 'router/account.php';
+
