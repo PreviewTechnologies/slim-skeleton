fd7898b636e9a209ed752c2a3226ef44cabb33fc
sohelrana820 <me.sohelrana@gmail.com>
2016-10-24 17:40:01 +0600

    Make way yo inject css/js into layout from working view file!

--- src/router/account.php
+++ src/router/account.php
@@ -3,5 +3,5 @@
 $app->get('/[{name}]', function ($request, $response, $args) {
     $this->flash->addMessage('success', 'Slim Skeleton ready to Go!');
     $msg = $this->flash->getMessages();
-    return $this->view->render($response, 'index.twig', ['message' => $msg]);
+    return $this->view->render($response, 'home.twig', ['message' => $msg]);
 })->add($testMiddleware);
