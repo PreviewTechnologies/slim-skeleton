5deeadbd24f994945c9e94767e0abab05f57fddb
sohelrana820 <me.sohelrana@gmail.com>
2016-10-24 17:34:48 +0600

    Test middleware is configured!

--- src/middleware.php
+++ src/middleware.php
@@ -2,3 +2,7 @@
 // Application middleware
 
 // e.g: $app->add(new \Slim\Csrf\Guard);
+
+$testMiddleware = function ($request, $response, $next) {
+    return $next($request, $response);
+};
--- src/router/account.php
+++ src/router/account.php
@@ -4,4 +4,4 @@ $app->get('/[{name}]', function ($request, $response, $args) {
     $this->flash->addMessage('success', 'Slim Skeleton ready to Go!');
     $msg = $this->flash->getMessages();
     return $this->view->render($response, 'index.twig', ['message' => $msg]);
-});
+})->add($testMiddleware);
